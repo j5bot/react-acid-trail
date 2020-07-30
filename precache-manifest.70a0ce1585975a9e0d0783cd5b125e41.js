ba42429fe2eae7678c8dbe936001630a6c19560e
Jonathan 'J5' Cook <Jonathan.Cook@logianalytics.com>
2020-07-30 12:58:39 -0400

    Updates

---
+++ precache-manifest.70a0ce1585975a9e0d0783cd5b125e41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58520da2d040a1153729",
+    "url": "/react-acid-trail/static/js/runtime~main.58520da2.js"
+  },
+  {
+    "revision": "dd4e6de12b582d42d5f3",
+    "url": "/react-acid-trail/static/js/main.dd4e6de1.chunk.js"
+  },
+  {
+    "revision": "fb3edf28f45ef4dd19cd",
+    "url": "/react-acid-trail/static/js/1.fb3edf28.chunk.js"
+  },
+  {
+    "revision": "dd4e6de12b582d42d5f3",
+    "url": "/react-acid-trail/static/css/main.c45138df.chunk.css"
+  },
+  {
+    "revision": "fb3edf28f45ef4dd19cd",
+    "url": "/react-acid-trail/static/css/1.770b91f9.chunk.css"
+  },
+  {
+    "revision": "bb22395189becc01eb8ccd1c5409e35f",
+    "url": "/react-acid-trail/index.html"
+  }
+];
